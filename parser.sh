7d6fad44b33ce110ab06212d41dba0d57dc6f328
stefano <stefano.romeo@studio.unibo.it>
2022-03-29 17:19:30 +0200

    added a parser

---
+++ parser.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ "$1" == "-h" ]]
+then
+    echo "$0 FILENAME PARAM WORD ONLY_VAL(TRUE/FALSE)"
+    echo "es:"
+    echo "./parser.sh ./output_temp_stresser.txt RESPONSE_TIME request"
+    echo "./parser.sh ./output_temp_stresser.txt RESPONSE_TIME latency"
+    echo "./parser.sh ./output_temp_stresser.txt RESPONSE_TIME latency true"
+    exit
+fi
+
+FILENAME="$1"
+PARAM="$2"
+WORD="$3"
+ONLY_VAL="$4"
+
+if [[ $ONLY_VAL == "true" ]]
+then
+    cat output_temp_invoker.txt| grep DB_LATENCY |awk -F "$WORD " '{print $2}' | awk -F " " '{print $2}'
+elif [[ $WORD != "" ]]
+then
+    cat output_temp_invoker.txt| grep DB_LATENCY |awk -F "$WORD " '{print $2}'
+else
+    cat "$FILENAME"| grep "$PARAM"
+fi
+
+## ./parser.sh ./output_temp_stresser.txt RESPONSE_TIME request
+## ./parser.sh ./output_temp_stresser.txt RESPONSE_TIME latency
+## ./parser.sh ./output_temp_stresser.txt RESPONSE_TIME latency true
+##cat output_temp_invoker.txt| grep DB_LATENCY | sed -n -e 's/^.*latency //p'
